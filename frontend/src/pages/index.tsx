9d0dc967c4ce0a53651fe591cdbb6289c53d150a
Benjamin von Polheim <ben@vp.solutions>
2020-03-20 22:09:08 +0100

    move to frontend folder

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -29,7 +29,7 @@ const Home: React.FC = () => {
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
 
-      <main className="max-w-7xl mx-auto">
+      <main className="max-w-7xl mx-auto p-4">
         <div className="bg-white shadow sm:rounded-lg">
           <div className="px-4 py-5 sm:p-6">
             <h3 className="text-lg leading-6 font-medium text-gray-900">Delete your account</h3>
