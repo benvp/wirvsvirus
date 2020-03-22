c33d1d6e14b8d622378c0a40d5080a8c0a99c85e
Benjamin von Polheim <ben@vp.solutions>
2020-03-22 16:55:37 +0100

    add google analytics

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,7 +5,23 @@ import { AuthProvider } from '../context/AuthContext';
 import 'react-datepicker/dist/react-datepicker.css';
 import '@@/scss/react-tag-autocomplete/styles.scss';
 
+import ReactGA from 'react-ga';
+import { useEffect } from 'react';
+
+const initGoogleAnalytics = () => {
+  ReactGA.initialize('UA-106811947-6');
+};
+
 export default function MyApp({ Component, pageProps }: any) {
+  useEffect(() => {
+    if (!(window as any).__GA_INITIALIZED) {
+      initGoogleAnalytics();
+      (window as any).__GA_INITIALIZED = true;
+    }
+
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <AuthProvider>
       <Shell>
