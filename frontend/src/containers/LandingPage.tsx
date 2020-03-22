ba80aa18ff77bef839b75407db5797d79bee9a0f
Benjamin von Polheim <ben@vp.solutions>
2020-03-22 17:51:40 +0100

    fix mobile menu

--- frontend/src/containers/LandingPage.tsx
+++ frontend/src/containers/LandingPage.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { ROUTES } from '@@modules/routes';
 
 type LandingPageProps = {};
 
 export const LandingPage: React.FC<LandingPageProps> = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div>
       <div className="relative bg-white overflow-hidden">
@@ -19,6 +21,7 @@ export const LandingPage: React.FC<LandingPageProps> = () => {
                     </a>
                     <div className="-mr-2 flex items-center md:hidden">
                       <button
+                        onClick={() => setMenuOpen(s => !s)}
                         type="button"
                         className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                       >
@@ -55,7 +58,13 @@ export const LandingPage: React.FC<LandingPageProps> = () => {
               </nav>
             </div>
 
-            <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right hidden">
+            <div
+              className={
+                menuOpen
+                  ? 'absolute top-0 inset-x-0 p-2 transition transform origin-top-right'
+                  : 'absolute top-0 inset-x-0 p-2 transition transform origin-top-right hidden'
+              }
+            >
               <div className="rounded-lg shadow-md">
                 <div className="rounded-lg bg-white shadow-xs overflow-hidden">
                   <div className="px-5 pt-4 flex items-center justify-between">
@@ -68,6 +77,7 @@ export const LandingPage: React.FC<LandingPageProps> = () => {
                     </div>
                     <div className="-mr-2">
                       <button
+                        onClick={() => setMenuOpen(s => !s)}
                         type="button"
                         className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                       >
@@ -88,38 +98,16 @@ export const LandingPage: React.FC<LandingPageProps> = () => {
                     </div>
                   </div>
                   <div className="px-2 pt-2 pb-3">
-                    <a
-                      href="#"
-                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                    >
-                      Product
-                    </a>
-                    <a
-                      href="#"
-                      className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                    >
-                      Features
-                    </a>
-                    <a
-                      href="#"
-                      className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                    >
-                      Marketplace
-                    </a>
-                    <a
-                      href="#"
-                      className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                    >
-                      Company
-                    </a>
-                  </div>
-                  <div>
-                    <a
-                      href="#"
-                      className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100 hover:text-indigo-700 focus:outline-none focus:bg-gray-100 focus:text-indigo-700 transition duration-150 ease-in-out"
-                    >
-                      Log in
-                    </a>
+                    <Link href={ROUTES.TRAININGS}>
+                      <a className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out">
+                        Trainings
+                      </a>
+                    </Link>
+                    <Link href={ROUTES.SIGN_UP}>
+                      <a className="mt-1 block text-indigo-700 px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out">
+                        Jetzt mitmachen
+                      </a>
+                    </Link>
                   </div>
                 </div>
               </div>
--- frontend/src/containers/Shell.tsx
+++ frontend/src/containers/Shell.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { ROUTES, PUBLIC_ROUTES, DYNAMIC_ROUTES } from '../modules/routes';
@@ -33,6 +33,8 @@ export const Shell: React.FC<ShellProps> = ({ children }) => {
   const router = useRouter();
 
   const { route } = router;
+
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const isPublic = PUBLIC_ROUTES.includes(route);
 
@@ -110,7 +112,10 @@ export const Shell: React.FC<ShellProps> = ({ children }) => {
               )}
             </div>
             <div className="-mr-2 flex items-center sm:hidden">
-              <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+              <button
+                onClick={() => setMenuOpen(open => !open)}
+                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+              >
                 <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                   <path
                     className="inline-flex"
@@ -131,55 +136,31 @@ export const Shell: React.FC<ShellProps> = ({ children }) => {
             </div>
           </div>
         </div>
-        <div className="hidden sm:hidden">
+        <div className={menuOpen ? 'sm:hidden' : 'hidden sm:hidden'}>
           <div className="pt-2 pb-3">
-            <a
-              href="#"
-              className="block pl-3 pr-4 py-2 border-l-4 border-indigo-500 text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out"
-            >
-              Login
-            </a>
-            <a
-              href="#"
-              className="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out"
-            >
-              Trainings
-            </a>
-          </div>
-          <div className="pt-4 pb-3 border-t border-gray-200">
-            <div className="flex items-center px-4">
-              <div className="flex-shrink-0">
-                <img
-                  className="h-10 w-10 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-              </div>
-              <div className="ml-3">
-                <div className="text-base font-medium leading-6 text-gray-800">Tom Cook</div>
-                <div className="text-sm font-medium leading-5 text-gray-500">tom@example.com</div>
-              </div>
-            </div>
-            <div className="mt-3">
-              <a
-                href="#"
-                className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out"
-              >
-                Your Profile
-              </a>
-              <a
-                href="#"
-                className="mt-1 block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out"
-              >
-                Settings
-              </a>
-              <a
-                href="#"
-                className="mt-1 block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out"
-              >
-                Sign out
-              </a>
-            </div>
+            {user ? (
+              <React.Fragment>
+                <button
+                  className="ml-6 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                  type="button"
+                  onClick={() => {
+                    logout();
+                    router.replace(ROUTES.LOGIN);
+                  }}
+                >
+                  Abmelden
+                </button>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <NavLink href={ROUTES.SIGN_UP} active={route === ROUTES.SIGN_UP}>
+                  Registrieren
+                </NavLink>
+                <NavLink className="ml-6" href={ROUTES.LOGIN} active={route === ROUTES.LOGIN}>
+                  Login
+                </NavLink>
+              </React.Fragment>
+            )}
           </div>
         </div>
       </nav>
