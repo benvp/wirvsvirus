960cbaec8a096f2b7f62786b8a3fef2cd01db13b
Frédéric Bolvin <frederic@bol.vin>
2020-03-22 15:43:57 +0100

    only seed admin user and tags

--- backend/src/seeders/seeder.ts
+++ backend/src/seeders/seeder.ts
@@ -33,54 +33,56 @@ export class Seeder {
     const hasTags = await this.tagRepository.hasTags();
 
     if (!hasUsers && !hasTrainings && !hasTags) {
-        this.logger.log(
-            'No users, trainings and tags found. Seeding...',
-        );
+      this.logger.log(
+        'No users, trainings and tags found. Seeding...',
+      );
 
-        const adminPwd = process.env.ADMIN_PASSWORD;
-        if (!adminPwd)
-          throw new Error('Please provide an initial admin password with the environment variable ADMIN_PASSWORD!');
+      const adminPwd = process.env.ADMIN_PASSWORD;
+      if (!adminPwd)
+        throw new Error('Please provide an initial admin password with the environment variable ADMIN_PASSWORD!');
 
-        // Admin User
-        const credentials = new AuthCredentialsDto();
-        credentials.username = 'admin';
-        credentials.password = adminPwd;
-        credentials.displayName = 'admin';
-        credentials.profilePicture = 'placeholder_' + String(Math.floor(Math.random() * Math.floor(4)))
+      // Admin User
+      const credentials = new AuthCredentialsDto();
+      credentials.username = 'admin';
+      credentials.password = adminPwd;
+      credentials.displayName = 'admin';
+      credentials.profilePicture = 'placeholder_' + String(Math.floor(Math.random() * Math.floor(4)))
 
-        await this.uRepository.signUp(credentials, Role.ADMIN);
-        
-        // Trainings and Hosts
-        for (const seed of seedTrainings) {
-            const train = new CreateTrainingDto();
-            train.name = seed.name
-            train.date = new Date(0)
-            train.date.setUTCSeconds(seed.date)
-            train.description = seed.description
-            train.youtubeVideo = seed.youtubeVideo
-            train.pictureLink = seed.pictureLink
-            train.professional = seed.professional
+      await this.uRepository.signUp(credentials, Role.ADMIN);
 
-            // Mock Users
-            const creds = new AuthCredentialsDto();
-            creds.username = seed.host
-            creds.password = seed.host + '1234';
-            creds.displayName = seed.host;
-            creds.profilePicture = 'placeholder_' + String(Math.floor(Math.random() * Math.floor(4)))
-            await this.uRepository.signUp(creds, Role.TRIMMER);
+      // Trainings and Hosts
+      // await this.seedSampleUsersAndTrainings();
 
-            const user = await this.uRepository.getUserByName(seed.host)
-            await this.tService.createTraining(train, user);
-        }
-
-        // Tags...
-        for (const t of tags) {
-            const tag = new CreateTagDto()
-            tag.text = t
-            await this.tagRepository.createTag(tag)
-        }
-
+      // Tags...
+      for (const t of tags) {
+        const tag = new CreateTagDto()
+        tag.text = t
+        await this.tagRepository.createTag(tag)
+      }
     }
+  }
 
+  seedSampleUsersAndTrainings = async () => {
+    for (const seed of seedTrainings) {
+      const train = new CreateTrainingDto();
+      train.name = seed.name
+      train.date = new Date(0)
+      train.date.setUTCSeconds(seed.date)
+      train.description = seed.description
+      train.youtubeVideo = seed.youtubeVideo
+      train.pictureLink = seed.pictureLink
+      train.professional = seed.professional
+
+      // Mock Users
+      const creds = new AuthCredentialsDto();
+      creds.username = seed.host
+      creds.password = seed.host + '1234';
+      creds.displayName = seed.host;
+      creds.profilePicture = 'placeholder_' + String(Math.floor(Math.random() * Math.floor(4)))
+      await this.uRepository.signUp(creds, Role.TRIMMER);
+
+      const user = await this.uRepository.getUserByName(seed.host)
+      await this.tService.createTraining(train, user);
+    }
   }
 }
